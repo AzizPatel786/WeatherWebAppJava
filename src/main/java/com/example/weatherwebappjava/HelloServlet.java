bc998a2a907cf653172d935f78c9d33ba2f2f371
AzizP786 <m.azizp23@gmail.com>
2024-03-02 08:52:58 +1300

    Integrated API.

--- src/main/java/com/example/weatherwebappjava/HelloServlet.java
+++ src/main/java/com/example/weatherwebappjava/HelloServlet.java
@@ -1,6 +1,9 @@
 package com.example.weatherwebappjava;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -26,6 +29,11 @@ public class HelloServlet extends HttpServlet {
         String myApiKey = "d89da33fb3f74f43e37a2f1755a7d546";
         String city = request.getParameter("city"); // Get the city from input
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myApiKey;
+
+        // Integrate API
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // Establishing connection
+        connection.setRequestMethod("GET"); // Get the data with the connection
     }
 
     public void destroy() {
