645861a265ba282d445eb9fd66552b8224efa106
AzizP786 <m.azizp23@gmail.com>
2024-03-04 18:48:09 +1300

    Weather data is displayed on screen according to selected city.

--- src/main/java/com/example/weatherwebappjava/HelloServlet.java
+++ src/main/java/com/example/weatherwebappjava/HelloServlet.java
@@ -34,54 +34,57 @@ public class HelloServlet extends HttpServlet {
         String myApiKey = "d89da33fb3f74f43e37a2f1755a7d546";
         String city = request.getParameter("city"); // Get the city from input
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myApiKey;
+        try {
+            // Integrate API
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // Establishing connection
+            connection.setRequestMethod("GET"); // Get the data with the connection
 
-        // Integrate API
-        URL url = new URL(apiUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // Establishing connection
-        connection.setRequestMethod("GET"); // Get the data with the connection
+            // Read data from network
+            InputStream inputStream = connection.getInputStream();
+            InputStreamReader streamReader = new InputStreamReader(inputStream);
 
-        // Read data from network
-        InputStream inputStream = connection.getInputStream();
-        InputStreamReader streamReader = new InputStreamReader(inputStream);
+            // Store the data that is read
+            StringBuilder responseContent = new StringBuilder();
 
-        // Store the data that is read
-        StringBuilder responseContent = new StringBuilder();
+            // Scan the data from the reader line by line
+            Scanner scanner = new Scanner(streamReader);
+            while (scanner.hasNext()) {
+                responseContent.append(scanner.nextLine());
+            }
+            scanner.close();
 
-        // Scan the data from the reader line by line
-        Scanner scanner = new Scanner(streamReader);
-        while (scanner.hasNext()) {
-            responseContent.append(scanner.nextLine());
+            // Parse weather data from string to JSON
+            Gson gson = new Gson(); // Allows the JSON data into tree model
+            JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
+
+            // Extracting Date and Time
+            long dateTimeStamp = jsonObject.get("dt").getAsLong() * 1000; //ms to sec
+            String date = new Date(dateTimeStamp).toString();
+            // Extracting temperature
+            double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble(); // In the main object there is another temp obj
+            int temperatureCelsius = (int) (temperatureKelvin - 273.15);
+            // Extracting Humidity
+            int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+            // Extracting Wind Speed
+            double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+            // Extracting Weather Condition
+            String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").toString();
+            // Set the data as request attributes (for sending to the jsp page)
+            request.setAttribute("date", date);
+            request.setAttribute("city", city);
+            request.setAttribute("temperature", temperatureCelsius);
+            request.setAttribute("weatherCondition", weatherCondition);
+            request.setAttribute("humidity", humidity);
+            request.setAttribute("windSpeed", windSpeed);
+            request.setAttribute("weatherData", responseContent.toString());
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        scanner.close();
-
-        // Parse weather data from string to JSON
-        Gson gson = new Gson(); // Allows the JSON data into tree model
-        JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
-
-        // Extracting Date and Time
-        long dateTimeStamp = jsonObject.get("dt").getAsLong() * 1000; //ms to sec
-        String date = new Date(dateTimeStamp).toString();
-        // Extracting temperature
-        double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble(); // In the main object there is another temp obj
-        int temperatureCelsius = (int) (temperatureKelvin - 273.15);
-        // Extracting Humidity
-        int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
-        // Extracting Wind Speed
-        double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
-        // Extracting Weather Condition
-        String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").toString();
-        // Set the data as request attributes (for sending to the jsp page)
-        request.setAttribute("date", date);
-        request.setAttribute("city", city);
-        request.setAttribute("temperature", temperatureCelsius);
-        request.setAttribute("weatherCondition", weatherCondition);
-        request.setAttribute("humidity", humidity);
-        request.setAttribute("windSpeed", windSpeed);
-        request.setAttribute("weatherData", responseContent.toString());
-
         // Forward the request to the weather.jsp page for rendering
         request.getRequestDispatcher("index.jsp").forward(request, response);
-        connection.disconnect();
+
     }
 
 //    private static InputStreamReader getInputStreamReader(String city, String myApiKey) throws IOException {
